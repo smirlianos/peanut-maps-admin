56058d76a020d2b1cc6ba0cb625a2a149bb9874a
Andonis Smirlianos <asmyrlia@netcompany.com>
2024-11-21 15:37:11 +0200

    test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static("public"));
 
 // Example API route
 app.get("/api/articles", (req, res) => {
-    res.json({ message: "Fetching all articles" });
+    res.json({ message: "Fetching all articles YOOO" });
 });
 
 // Catch-all route for static files
