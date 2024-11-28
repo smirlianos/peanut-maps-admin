2e76a3208479aa451b8b57169cfe48a8559ad522
Andonis Smirlianos <asmyrlia@netcompany.com>
2024-11-28 11:04:32 +0200

    test

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -4,16 +4,15 @@ exports.sendTestEmail = async (req, res) => {
     const { to, subject, text, html } = req.body;
 
     if (!to || !subject || (!text && !html)) {
-        return res
-            .status(400)
-            .json({
-                error: "Missing required fields: to, subject, text or html",
-            });
+        return res.status(400).json({
+            error: "Missing required fields: to, subject, text or html",
+        });
     }
 
     try {
-        await sendEmail({ to, subject, text, html });
         res.status(200).json({ message: "Email sent successfully" });
+        //await sendEmail({ to, subject, text, html });
+        //res.status(200).json({ message: "Email sent successfully" });
     } catch (error) {
         res.status(500).json({ error: "Failed to send email" });
     }
