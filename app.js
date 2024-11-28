4dee4a7886bd7e87664d2269ed0c20578ab8af3e
Andonis Smirlianos <asmyrlia@netcompany.com>
2024-11-28 10:57:29 +0200

    misc

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
     cors({
-        origin: "http://localhost:3000", // Replace with your frontend URL
+        origin: "http://localhost:5173", // Replace with your frontend URL
         methods: ["GET", "POST", "PATCH", "PUT", "DELETE"], // Allowed HTTP methods
         allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
     })
